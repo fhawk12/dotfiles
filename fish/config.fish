c58a7a7c1f5600a58176d6530ca3d26f8e196bf4
fhawk12 <2369187058@qq.com>
2023-08-04 22:45:45 +0800

    init

---
+++ fish/config.fish
@@ -0,0 +1,21 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export EDITOR=nvim
+set -gx PATH /home/fhawk/.local/bin $PATH
+
+alias ra=ranger
+alias gc="git clone "
+alias fh=neofetch
+alias keymap="cd ~; xmodmap .Xmodmap"
+alias colemak="setxkbmap us -variant colemak"
+alias us="setxkbmap us"
+alias lg=lazygit
+
+alias pro="export https_proxy=http://127.0.0.1:7890;export http_proxy=http://127.0.0.1:7890;export all_proxy=socks5://127.0.0.1:7890"
+
+if [ -z "$DISPLAY" ] && [ "$XDG_VTNR" -eq 1 ]
+    exec startx
+end
+
