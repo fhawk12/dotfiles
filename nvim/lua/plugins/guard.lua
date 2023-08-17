09887a8ab99401637fccb5dd4daba97789c4a5b0
fhawk12 <2369187058@qq.com>
2023-08-17 11:41:13 +0800

    update

--- nvim/lua/plugins/guard.lua
+++ nvim/lua/plugins/guard.lua
@@ -1,24 +1,24 @@
 return {
-	"nvimdev/guard.nvim",
-	keys = {
-		{ "<leader>fm", "<cmd>GuardFmt<cr>", mode = { "n", "v" } },
-	},
+  "nvimdev/guard.nvim",
+  keys = {
+    { "<leader>fm", "<cmd>GuardFmt<cr>", mode = { "n", "v" } },
+  },
   lazy = "VeryLazy",
   enabled = true,
-	config = function()
-		local ft = require("guard.filetype")
-		ft("c"):fmt("clang-format")
-		ft("cpp"):fmt("clang-format")
-		ft("lua"):fmt("stylua")
-		ft("python"):fmt("black")
-		ft("sh"):fmt("lsp")
-		ft("markdown"):fmt("prettier")
-		ft("typescript,javascript,typescriptreact"):fmt("prettier")
-		require("guard").setup({
-			-- the only options for the setup function
-			fmt_on_save = false,
-			-- Use lsp if no formatter was defined for this filetype
-			lsp_as_default_formatter = true,
-		})
-	end,
+  config = function()
+    local ft = require("guard.filetype")
+    ft("c"):fmt("clang-format")
+    ft("cpp"):fmt("clang-format")
+    ft("lua"):fmt("stylua")
+    ft("python"):fmt("black")
+    ft("sh"):fmt("lsp")
+    ft("markdown"):fmt("prettier")
+    ft("typescript,javascript,typescriptreact"):fmt("prettier")
+    require("guard").setup({
+      -- the only options for the setup function
+      fmt_on_save = false,
+      -- Use lsp if no formatter was defined for this filetype
+      lsp_as_default_formatter = true,
+    })
+  end,
 }
